134509eddfb158fccc53b7af9159d0067deeeeb1
Ipshita Chatterjee <ipshita1996@gmail.com>
2018-12-08 14:44:26 +0530

    adding on click events

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,3 @@
-var editField =  document.querySelector('#numText');
-//editField.value = "Hola";
-var numbers = document.querySelectorAll(".numbers p");
-var numArray = Array.apply(null, numbers);
-numArray.forEach(element => {
-    console.log(element)
-});
 var add = function(num1 , num2) {
     return num1+num2;
 }
@@ -28,4 +21,35 @@ var operate = function(operator, num1, num2) {
         return divide(num1,num2);
     }
 }
-console.log(operate("*",2,3));
+
+//adding numbers to textfield
+var editField =  document.querySelector('#numText');
+//editField.value = "1 + 2 - 3";
+var numbers = document.querySelectorAll(".numbers p");
+var operators = document.querySelectorAll(".operations p");
+var numArray = Array.apply(null, numbers);
+var opArray = Array.apply(null,operators);
+
+numArray.forEach(element => {
+    element.addEventListener("click", function(){
+        console.log(element.innerHTML);
+        editField.value += element.innerHTML;
+    });
+});
+opArray.forEach(element => {
+    element.addEventListener("click", function(){
+        console.log(element.innerHTML);
+        if(editField.value != ""){
+            editField.value += " "+element.innerHTML+" ";
+        }
+    });
+});
+//first operator should always be a number
+//add same functionality for operators as well (if two operators clicked on a row the overwrite the most recent operator)
+//modify DEL button
+
+//the clear button
+var clearButton = document.querySelector(".calc-field span");
+clearButton.addEventListener("click", function() {
+    editField.value = "";
+});
